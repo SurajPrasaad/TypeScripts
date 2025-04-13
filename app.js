60bef3278792f6a1e7274ca9b5e9e6d396b92ef6
SurajPrasaad <surajkameshwarprasad@gmail.com>
2025-04-13 20:17:18 +0530

    Overview of Typescripts

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 console.log("Hello TypeScripts");
+
+function add(x, y) {
+  console.log(x + y);
+}
+
+add(3, 4);
+add("Suraj", "Kumar");
+
+function toCreateUser(user) {
+  console.log(user.firstName, user.lastName, user.age);
+}
+
+toCreateUser({
+  firstName: "Suraj",
+  lastName: "Kumar",
+  age: 18,
+});
+
+// JavaScript - Loosely Typed Language - means type not defined
+
+let x = 10
+x = "Suraj"
+ x = true
+
+// Typescripts = Javascript + Types
